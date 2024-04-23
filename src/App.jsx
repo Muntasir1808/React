1135d93f18beefed53b4204dbc2c3d12191c65ae
Muntasir <muntasirsunny05@gmail.com>
2024-04-23 19:29:17 +0600

    Storing component style files next to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // importing images are necessary for deployment, if not then image won't load
 import componentsImg from "./assets/components.png";
 import { CORE_CONCEPTS } from './data.js';
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 
 
