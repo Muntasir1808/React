a67c2566fb93d7baf358b043fa5594e2acd6e70d
Muntasir <muntasirsunny05@gmail.com>
2024-04-24 15:05:52 +0600

    showing active button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 // importing images are necessary for deployment, if not then image won't load
+import { useState } from "react";
 import componentsImg from "./assets/components.png";
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
-
-
-
+import { EXAMPLES } from "./data.js";
 
 function App() {
+  // we must call react hooks inside of a component function and we must call it at the top level of the component function
+  const [selectedTopic, setSelectedTopic] = useState();
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
-    console.log(selectedButton);
+    // console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  console.log("APP COMPONENT EXECUTING");
+  let tabContent = <p>Please select a topic.</p>
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
   return (
     <div>
@@ -37,14 +55,45 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={()=>{handleSelect('components')}}>Components</TabButton>
+            {/* <TabButton>Components</TabButton> */}
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => {
+                handleSelect("components");
+              }}
+            >
+              Components
+            </TabButton>
             {/* we can also write a label prop like this. See in the TabButton.jsx*/}
             {/* <TabButton label='Components'/>  */}
-            <TabButton onSelect={()=>{handleSelect('jsx')}}>JSX</TabButton>
-            <TabButton onSelect={()=>{handleSelect('props')}}>Props</TabButton>
-            <TabButton onSelect={()=>{handleSelect('state')}}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => {
+                handleSelect("jsx");
+              }}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => {
+                handleSelect("props");
+              }}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => {
+                handleSelect("state");
+              }}
+            >
+              State
+            </TabButton>
           </menu>
-          
+          {/* Dynamic Content */}
+          {/* {selectedTopic}; */}
+          {tabContent}
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -13,12 +13,12 @@
 //       </li>
 //     );
 // }
-export default function TabButton({children, onSelect}) {
-    
+export default function TabButton({children, onSelect, isSelected}) {
+    console.log('TABBUTTON COMPONENT EXECUTING');
     return (
       <li>
         {/* the value of any event prop is a function */}
-        <button onClick={onSelect}>{children}</button>
+        <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
       </li>
     );
 }
